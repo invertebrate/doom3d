7b4d38a3efbc6cc137e35ff086fc5a106e5563bd
hakolao <okkohakola@gmail.com>
2021-01-02 17:26:36 +0200

    For now, remove temp menus for which functionality does not yet exist

--- src/scene/menus/editor3d_menu.c
+++ src/scene/menus/editor3d_menu.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 00:07:43 by ohakola           #+#    #+#             */
-/*   Updated: 2021/01/02 17:18:22 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/01/02 17:26:02 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -170,17 +170,11 @@ static void			on_editor_menu_button_click(t_button *self, void *params)
 	else
 	{
 		if (self->id == 2)
-			new_menu_id = editor_menu_prefabs;
-		else if (self->id == 3)
 			new_menu_id = editor_menu_objects;
-		else if (self->id == 4)
+		else if (self->id == 3)
 			new_menu_id = editor_menu_textures;
-		else if (self->id == 5)
+		else if (self->id == 4)
 			new_menu_id = editor_menu_normalmaps;
-		else if (self->id == 6)
-			new_menu_id = editor_menu_triggers;
-		else if (self->id == 7)
-			new_menu_id = editor_menu_enemies;
 		create_or_open_popup_menu(app, new_menu_id, self);
 	}
 }
@@ -189,17 +183,13 @@ void				editor3d_menu_create(t_doom3d *app)
 {
 	app->active_scene->menus[0] = button_menu_create(app,
 		(t_button_menu_params){
-			.button_names = (const char*[8]){
+			.button_names = (const char*[5]){
 				"Exit",
 				"Save",
-				"Prefabs",
 				"Objects",
 				"Textures",
-				"NormMaps",
-				"Triggers",
-				"Enemies"
-				},
-			.num_buttons = 8,
+				"NormMaps"},
+			.num_buttons = 5,
 			.on_click = on_editor_menu_button_click,
 			.button_font = app->window->main_font,
 		});
