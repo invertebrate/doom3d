f7bad9c5de8e09cdb0fff9b6f7796675e5da001d
SotaMursu <you@example.com>
2021-01-11 12:55:49 +0100

    glock firerate 4 -> 6.66

--- src/inventory/item_data/item_data_glock.c
+++ src/inventory/item_data/item_data_glock.c
@@ -6,7 +6,7 @@
 /*   By: ahakanen <aleksi.hakanen94@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 12:04:24 by ahakanen          #+#    #+#             */
-/*   Updated: 2021/01/11 12:11:34 by ahakanen         ###   ########.fr       */
+/*   Updated: 2021/01/11 12:55:16 by ahakanen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_item	item_data_glock(t_doom3d *app)
 	item.item_type = item_type_sidearm;
 	item.ammo = 20;
 	item.fire_type = fire_ray;
-	item.fire_rate = 4;
+	item.fire_rate = 6.66;
 	item.range = 80 * app->unit_size;
 	item.damage = 20;
 	return (item);
