411704f14e70eea4b8581f1aa06a148d59a36793
SotaMursu <you@example.com>
2021-01-13 13:27:28 +0100

    player status, mostly just hp and player taking damage

--- src/npc/npc_update.c
+++ src/npc/npc_update.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   npc_update.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: ahakanen <aleksi.hakanen94@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 17:21:49 by ahakanen          #+#    #+#             */
-/*   Updated: 2021/01/06 16:12:23 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/01/13 13:25:28 by ahakanen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ static void	handle_atk_anim(t_doom3d *app, t_3d_object *npc_obj)
 {
 	uint32_t		new_time;
 	t_npc			*npc;
+	t_vec3			dist;
 
 	(void)app; //!ToDo: Remove from params if app is never needed...
 	npc = npc_obj->params;
@@ -24,6 +25,9 @@ static void	handle_atk_anim(t_doom3d *app, t_3d_object *npc_obj)
 	if (npc->atk_timer > npc->atk_dur)
 	{
 		ft_printf("npc %d attacked!\n", npc_obj->id);//test
+		ml_vector3_sub(app->player.pos, npc_obj->position, dist);
+		if (ml_vector3_mag(dist) < npc->atk_range)
+			player_onhit(app, npc->atk_dmg);
 		npc->state = state_attack;
 	}
 }
--- src/player/player_status.c
+++ src/player/player_status.c
@@ -1,29 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   npc_default.c                                      :+:      :+:    :+:   */
+/*   player_status.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahakanen <aleksi.hakanen94@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/23 12:08:04 by ahakanen          #+#    #+#             */
-/*   Updated: 2021/01/13 13:25:59 by ahakanen         ###   ########.fr       */
+/*   Created: 2021/01/13 12:53:53 by ahakanen          #+#    #+#             */
+/*   Updated: 2021/01/13 13:10:31 by ahakanen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom3d.h"
 
-void	npc_default(t_doom3d *app, t_npc *npc)
+void	player_onhit(t_doom3d *app, int damage)
 {
-	npc->type = npc_type_default;
-	npc->speed = app->unit_size / 32;
-	npc->rot_speed = 10;
-	npc->state = 0;
-	npc->hp = 100;
-	npc->atk_range = app->unit_size * 5;
-	npc->atk_dmg = 10;
-	npc->atk_dur = 500;
-	npc->vision_range = app->unit_size * 10;
-	npc->model_key = NPC_DEFAULT_MODEL;
-	npc->texture_key = NPC_DEFAULT_TEXTURE;
-	npc->normal_map_key = NPC_DEFAULT_NORMM;
+	app->player.hp -= damage;
+	if (app->player.hp <= 0)
+	{
+		ft_printf("You died!\n"); //test
+	}
+	ft_printf("player hit for %d damage! current hp: %d\n",
+		damage, app->player.hp); //test
 }
