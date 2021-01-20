104af29789e677929d260624d45b5342c0a8a148
hakolao <okkohakola@gmail.com>
2021-01-20 21:35:25 +0200

    Only update triangle tree once per frame

--- src/object/object_update.c
+++ src/object/object_update.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 15:48:31 by ohakola           #+#    #+#             */
-/*   Updated: 2021/01/20 21:26:43 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/01/20 21:34:36 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@ static void		handle_object_deletions(t_doom3d *app)
 {
 	int32_t		i;
 	int32_t		del_index;
-	t_bool		deleted_something;
 	uint32_t	id;
 	char		obj_type[128];
 
 	l3d_temp_objects_destroy_if_expired(&app->active_scene->temp_objects);
 	i = -1;
-	deleted_something = false;
 	while (++i < (int32_t)app->active_scene->num_deleted)
 	{
 		del_index = app->active_scene->deleted_object_i[i];
@@ -33,11 +31,8 @@ static void		handle_object_deletions(t_doom3d *app)
 			ft_printf("Deleted %s, id %u\n", obj_type, id);
 			l3d_3d_object_destroy(app->active_scene->objects[del_index]);
 			app->active_scene->objects[del_index] = NULL;
-			deleted_something = true;
 		}
 	}
-	if (deleted_something)
-		active_scene_update_after_objects(app->active_scene);
 }
 
 static t_bool	should_update_npc_state(t_doom3d *app)
@@ -127,23 +122,25 @@ void			doom3d_update_objects(t_doom3d *app)
 	t_bool			is_npc_update;
 	t_3d_object		*obj;
 
-	if (app->active_scene->scene_id == scene_id_main_game ||
-		app->active_scene->scene_id == scene_id_editor3d)
-		handle_object_deletions(app);
-	if (app->active_scene->is_paused ||
-		app->active_scene->scene_id != scene_id_main_game)
+	if (!(app->active_scene->scene_id == scene_id_main_game ||
+		app->active_scene->scene_id == scene_id_editor3d))
 		return ;
-	is_npc_update = should_update_npc_state(app);
-	i = -1;
-	while (++i < (int32_t)(app->active_scene->num_objects +
-		app->active_scene->num_deleted))
+	if (!app->active_scene->is_paused &&
+		app->active_scene->scene_id == scene_id_main_game)
 	{
-		obj = app->active_scene->objects[i];
-		if (!obj)
-			continue ;
-		update_object_by_type(app, obj, is_npc_update);
+		is_npc_update = should_update_npc_state(app);
+		i = -1;
+		while (++i < (int32_t)(app->active_scene->num_objects +
+			app->active_scene->num_deleted))
+		{
+			obj = app->active_scene->objects[i];
+			if (!obj)
+				continue ;
+			update_object_by_type(app, obj, is_npc_update);
+		}
+		l3d_temp_objects_update_time(&app->active_scene->temp_objects,
+			app->info.delta_time);
 	}
-	l3d_temp_objects_update_time(&app->active_scene->temp_objects,
-		app->info.delta_time);
+	handle_object_deletions(app);
 	active_scene_update_after_objects(app->active_scene);
 }
