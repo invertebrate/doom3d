855f39466e8852f634d24cd50ff6b5377a233c96
hakolao <okkohakola@gmail.com>
2021-01-20 14:17:36 +0200

    Fix stupid stupid stupid mem leak :D

--- lib/lib3d/l3d_temp_objects.c
+++ lib/lib3d/l3d_temp_objects.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 18:25:34 by ohakola           #+#    #+#             */
-/*   Updated: 2021/01/19 22:36:43 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/01/20 14:16:40 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,40 +26,24 @@ static void			l3d_destroy_temp_object(void *params, size_t size)
 void				l3d_temp_objects_destroy_if_expired(
 						t_temp_objects **temp_objects)
 {
-	t_temp_objects	*curr;
-	t_temp_objects	*prev;
-	t_temp_objects	*tmp;
+	t_temp_objects	**curr;
+	t_temp_objects	*temp;
 	t_temp_object	*temp_obj;
 
-	curr = *temp_objects;
-	prev = NULL;
-	while (curr)
+	curr = temp_objects;
+	while (*curr)
 	{
-		temp_obj = curr->content;
+		temp = *curr;
+		temp_obj = temp->content;
 		if (temp_obj->lifetime <= 0)
 		{
-			if (prev)
-			{
-				prev->next = curr->next;
-				tmp = curr;
-				curr = curr->next;
-				free(tmp);
-				tmp = NULL;
-			}
-			else
-			{
-				curr = curr->next;
-				free(*temp_objects);
-				*temp_objects = NULL;
-				*temp_objects = curr;
-			}
+			*curr = temp->next != NULL ? temp->next : NULL;
+			free(temp);
+			temp = NULL;
 			l3d_destroy_temp_object(temp_obj, 0);
 		}
 		else
-		{
-			prev = curr;
-			curr = curr->next;
-		}
+			curr = &(*curr)->next;
 	}
 }
 
@@ -75,19 +59,17 @@ void				l3d_temp_objects_destroy(t_temp_objects **temp_objects)
 void				l3d_temp_objects_add(t_temp_objects **temp_objects,
 						t_3d_object *object, int32_t lifetime_and_delay[2])
 {
-	t_temp_object	*tmp_obj;
+	t_temp_object	tmp_obj;
 
-	error_check(!(tmp_obj = ft_calloc(sizeof(*tmp_obj))),
-		"Failed to malloc temp object");
-	tmp_obj->obj = object;
-	tmp_obj->lifetime = lifetime_and_delay[0];
-	tmp_obj->delay = lifetime_and_delay[1];
-	tmp_obj->obj->material->shading_opts |=
+	tmp_obj.obj = object;
+	tmp_obj.lifetime = lifetime_and_delay[0];
+	tmp_obj.delay = lifetime_and_delay[1];
+	tmp_obj.obj->material->shading_opts |=
 		e_shading_invisible | e_shading_zero_alpha;
 	if (*temp_objects == NULL)
-		*temp_objects = ft_lstnew(tmp_obj, sizeof(*tmp_obj));
+		*temp_objects = ft_lstnew(&tmp_obj, sizeof(tmp_obj));
 	else
-		ft_lstadd(temp_objects, ft_lstnew(tmp_obj, sizeof(*tmp_obj)));
+		ft_lstadd(temp_objects, ft_lstnew(&tmp_obj, sizeof(tmp_obj)));
 }
 
 /*
