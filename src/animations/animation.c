4435b451257051c5dd3356892197ef13c660b131
Vesa <vesa.eilo@hotmail.com>
2021-02-11 21:27:51 +0200

    removed print

--- src/animations/animation.c
+++ src/animations/animation.c
@@ -56,7 +56,6 @@ uint32_t				anim_3d_frame_update(t_doom3d *app, t_animation_3d *animation)
 		animation->tick_at_update = app->current_tick;
 	}
 	current_frame = animation->current_frame;
-	ft_printf("current_Frame %d\n", current_frame);
 	animation->current_object = animation->animation_frames[animation->current_frame];
 	npc_anim_3d_position_update(animation->base_object);
 	//npc_anim_3d_rotation_update(animation->base_object)
