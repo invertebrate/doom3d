af2c4deb27425190d2b065620f46a2a2b70e90c1
hakolao <okkohakola@gmail.com>
2021-02-27 16:12:03 +0200

    Allo multi-select rotation

--- src/events/mouse_state.c
+++ src/events/mouse_state.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 23:22:26 by ohakola           #+#    #+#             */
-/*   Updated: 2021/02/27 15:51:23 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/02/27 16:10:56 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,34 +51,39 @@ static void				object_rotation_handle(t_doom3d *app,
 	static uint32_t		last_rotated;
 	uint32_t			diff;
 	float				angle;
+	int32_t				i;
 
-	if (app->editor.num_selected_objects != 1)
+	if (app->editor.num_selected_objects == 0)
 		return ;
 	diff = SDL_GetTicks() - last_rotated;
-	if (app->keyboard.state[SDL_SCANCODE_Q] && diff > 50 && ft_abs(xrel) > 2)
+	i = -1;
+	while (++i < app->editor.num_selected_objects)
 	{
-		angle = (xrel > 0 ? 1 : -1) * 10;
-		l3d_3d_object_rotate(app->editor.selected_objects[0],
-			 0, angle, 0);
-		last_rotated = SDL_GetTicks();
-		if (app->editor.selected_objects[0]->type == object_type_npc)
-			((t_npc*)app->editor.selected_objects[0]->params)->angle += angle;
-		after_editor_transform(app, &last_rotated);
-	}
-	else if (diff > 50 && ft_abs(xrel) > 2 && ft_abs(yrel) < 8)
-	{
-		l3d_3d_object_rotate(app->editor.selected_objects[0],
-			 0, 0, (xrel > 0 ? 1 : -1) * 10);
-		last_rotated = SDL_GetTicks();
-		after_editor_transform(app, &last_rotated);
-	}
-	else if (diff > 50 && ft_abs(yrel) > 2 && ft_abs(xrel) < 8)
-	{
-		l3d_3d_object_rotate(app->editor.selected_objects[0],
-			(yrel > 0 ? 1 : -1) * 10, 0, 0);
-		last_rotated = SDL_GetTicks();
-		app->editor.is_saved = false;
-		after_editor_transform(app, &last_rotated);
+		if (app->keyboard.state[SDL_SCANCODE_Q] && diff > 50 && ft_abs(xrel) > 2)
+		{
+			angle = (xrel > 0 ? 1 : -1) * 10;
+			l3d_3d_object_rotate(app->editor.selected_objects[i],
+				0, angle, 0);
+			last_rotated = SDL_GetTicks();
+			if (app->editor.selected_objects[i]->type == object_type_npc)
+				((t_npc*)app->editor.selected_objects[i]->params)->angle += angle;
+			after_editor_transform(app, &last_rotated);
+		}
+		else if (diff > 50 && ft_abs(xrel) > 2 && ft_abs(yrel) < 8)
+		{
+			l3d_3d_object_rotate(app->editor.selected_objects[i],
+				0, 0, (xrel > 0 ? 1 : -1) * 10);
+			last_rotated = SDL_GetTicks();
+			after_editor_transform(app, &last_rotated);
+		}
+		else if (diff > 50 && ft_abs(yrel) > 2 && ft_abs(xrel) < 8)
+		{
+			l3d_3d_object_rotate(app->editor.selected_objects[i],
+				(yrel > 0 ? 1 : -1) * 10, 0, 0);
+			last_rotated = SDL_GetTicks();
+			app->editor.is_saved = false;
+			after_editor_transform(app, &last_rotated);
+		}
 	}
 }
 
@@ -101,7 +106,7 @@ static void				mouse_editor_state_handle(t_doom3d *app)
 		SDL_ShowCursor(SDL_ENABLE);
 		SDL_SetRelativeMouseMode(SDL_FALSE);
 		SDL_GetRelativeMouseState(&xrel, &yrel);
-		if (app->editor.num_selected_objects == 1 &&
+		if (app->editor.num_selected_objects > 0 &&
 			((app->mouse.state & SDL_BUTTON_MIDDLE) ||
 				(app->keyboard.state[SDL_SCANCODE_R])))
 		{
