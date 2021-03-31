65f9763753beaab490b1d3cc5f09a64f427a4f1e
hakolao <okkohakola@gmail.com>
2021-03-31 03:59:07 +0300

    Clean event codes

--- include/events.h
+++ include/events.h
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 15:09:16 by ohakola           #+#    #+#             */
-/*   Updated: 2021/03/31 02:43:41 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/03/31 03:56:50 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,30 @@
 
 typedef enum				e_doom3d_event
 {
-	event_object_delete = 111,
-	event_music_play = 222,
-	event_effect_play = 333,
-	event_editor_start_placement = 444,
-	event_editor_cancel_placement = 551,
-	event_editor_end_placement = 555,
-	event_editor_start_save = 1234,
-	event_editor_end_save = 4321,
-	event_editor_save_type = 1212,
-	event_editor_save_type_backspace = 1313,
-	event_editor_delete = 1414,
-	event_editor_select = 1818,
-	event_editor_deselect = 1919,
-	event_editor_level_switch = 2020,
-	event_editor_open_popup_menu = 2121,
-	event_editor_add_texture = 112233,
-	event_editor_add_normal_map = 113322,
-	event_editor_increment_patrol_slot = 553322,
-	event_editor_decrement_patrol_slot = 223322,
-	event_editor_exit = 1515,
-	event_window_resize = 666,
-	event_scene_change = 777,
-	event_scene_reload = 888,
-	event_quit = 999,
+	event_object_delete = 1,
+	event_music_play = 2,
+	event_effect_play = 3,
+	event_editor_start_placement = 4,
+	event_editor_cancel_placement = 5,
+	event_editor_end_placement = 6,
+	event_editor_start_save = 7,
+	event_editor_end_save = 8,
+	event_editor_save_type = 9,
+	event_editor_save_type_backspace = 10,
+	event_editor_delete = 11,
+	event_editor_select = 12,
+	event_editor_deselect = 13,
+	event_editor_level_switch = 14,
+	event_editor_open_popup_menu = 15,
+	event_editor_add_texture = 16,
+	event_editor_add_normal_map = 17,
+	event_editor_increment_patrol_slot = 18,
+	event_editor_decrement_patrol_slot = 19,
+	event_editor_exit = 20,
+	event_window_resize = 21,
+	event_scene_change = 22,
+	event_scene_reload = 23,
+	event_quit = 24,
 }							t_doom3d_event;
 
 # endif
