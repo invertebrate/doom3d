d4126cbfb2d8417772c2887a31eb053fb97d2793
SotaMursu <you@example.com>
2021-04-11 19:19:59 +0200

    point inside aabb fixed in lib3d, kind of working but bad collisions

--- src/player/collision.c
+++ src/player/collision.c
@@ -6,7 +6,7 @@
 /*   By: ahakanen <aleksi.hakanen94@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 23:22:26 by ohakola           #+#    #+#             */
-/*   Updated: 2021/04/11 03:47:26 by ahakanen         ###   ########.fr       */
+/*   Updated: 2021/04/11 19:19:11 by ahakanen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,27 @@
 ** collision-detection-smooth-wall-sliding-no-bounce-effect
 */
 
-static void		limit_movement_add_by_collision(t_vec3 collision_normal,
-					t_vec3 dir_add)
+static void		limit_movement_add_by_collision(t_doom3d *app,
+							t_hit *hit, t_vec3 dir, t_player future_player)
 {
-	t_vec3		direction_wall_part;
-
-	ml_vector3_mul(collision_normal,
-		ml_vector3_dot(dir_add, collision_normal), direction_wall_part);
-	if (direction_wall_part[1] > 0)
-		direction_wall_part[1] = 0;
-	ml_vector3_sub(dir_add, direction_wall_part, dir_add);
+	while (l3d_point_inside_aabb(&future_player.aabb, hit->hit_point))
+	{
+		ml_vector3_sub(app->player.pos, dir, app->player.pos);
+		ml_vector3_sub(future_player.pos, dir, future_player.pos);
+		player_update_aabb(&future_player);
+	}
+	ml_vector3_sub(app->player.pos, dir, app->player.pos);
+	player_update_aabb(&app->player);
 }
 
+static void		set_future_player(t_doom3d *app, t_vec3 add,
+					t_player *future_player)
+{
+	ft_memcpy(future_player, &app->player, sizeof(t_player));
+	ml_vector3_add(future_player->pos, add, future_player->pos);
+	player_update_aabb(future_player);
+}
+/*
 static t_hit	*l3d_get_aabb_hit_record_point(t_vec3 origin, t_box3d *target)
 {
 	t_vec3		dir;
@@ -47,25 +56,12 @@ static t_hit	*l3d_get_aabb_hit_record_point(t_vec3 origin, t_box3d *target)
 		return (hit);
 	}
 	return (NULL);
-}
-
-static void		set_future_player(t_doom3d *app, t_vec3 add,
-					t_player *future_player)
-{
-	t_vec3	new_add;
-
-	ml_vector3_add(add, 
-			(t_vec3) {0, -app->player.player_height / 4, 0}, new_add);
-	ft_memcpy(future_player, &app->player, sizeof(t_player));
-	ml_vector3_add(future_player->pos, new_add, future_player->pos);
-	player_update_aabb(future_player);
-}
+}*/
 
 void			collision_limit_player(t_doom3d *app, t_vec3 add)
 {
 	t_player	future_player;
 	t_hit		*closest_hit;
-	t_hit		*hit;
 	t_hits		*hits;
 	t_vec3		dir;
 
@@ -73,26 +69,12 @@ void			collision_limit_player(t_doom3d *app, t_vec3 add)
 		return ;
 	set_future_player(app, add, &future_player);
 	ml_vector3_normalize(add, dir);
-	if (l3d_kd_tree_ray_hits(app->active_scene->triangle_tree, app->player.pos,
+	if (l3d_kd_tree_ray_hits(app->active_scene->triangle_tree, app->player.aabb.center,
 		dir, &hits))
 	{
 		l3d_get_closest_triangle_hit(hits, &closest_hit, -1);
 		if (closest_hit)
-		{
-			if (l3d_point_inside_aabb(&future_player.aabb,
-											closest_hit->hit_point))
-			{
-				hit = l3d_get_aabb_hit_record_point(closest_hit->hit_point,
-													&future_player.aabb);
-				if (hit && closest_hit->triangle->parent->type != object_type_projectile &&
-					closest_hit->triangle->parent->type != object_type_trigger &&
-					closest_hit->triangle->parent->type != object_type_path)
-				{
-					limit_movement_add_by_collision(hit->normal, add);
-					free(hit);
-				}
-			}
-		}
+			limit_movement_add_by_collision(app, closest_hit, dir, future_player);
 		l3d_delete_hits(&hits);
 	}
 }
