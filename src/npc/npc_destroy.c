2779a793e15c8705321bb5b97b944ee6da0b68de
hakolao <okkohakola@gmail.com>
2021-04-14 23:49:27 +0300

    Revert void function back to bool

--- src/npc/npc_destroy.c
+++ src/npc/npc_destroy.c
@@ -6,18 +6,18 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 18:32:11 by veilo             #+#    #+#             */
-/*   Updated: 2021/04/14 23:11:56 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/04/14 23:49:03 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom3d.h"
 
-static void		animation_3d_destroy(t_animation_3d *anim)
+static t_bool		animation_3d_destroy(t_animation_3d *anim)
 {
 	uint32_t	i;
 
 	if (anim == NULL)
-		return ;
+		return (false);
 	i = 0;
 	while (i < ANIM_3D_FRAME_MAX)
 	{
@@ -30,15 +30,19 @@ static void		animation_3d_destroy(t_animation_3d *anim)
 	}
 	free(anim);
 	anim = NULL;
+	return (true);
 }
 
-t_bool			npc_destroy(t_3d_object *npc_obj)
-{
+t_bool				npc_destroy(t_3d_object *npc_obj)
+{	
 	if (npc_obj == NULL || npc_obj->type != object_type_npc)
 		return (false);
 	if (((t_npc*)npc_obj->params)->type == npc_type_default ||
 		((t_npc*)npc_obj->params)->type == npc_type_ranged)
-		animation_3d_destroy((((t_npc*)npc_obj->params))->animation_3d);
+	{
+		if (!animation_3d_destroy((((t_npc*)npc_obj->params))->animation_3d))
+			LOG_WARN("Failed to delete animation frames (NULL)");
+	}
 	l3d_3d_object_destroy(npc_obj);
 	return (true);
 }
