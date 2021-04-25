19bd220f25aaf63609027a2999e8bc43026b0b65
hakolao <okkohakola@gmail.com>
2021-04-25 17:22:29 +0300

    Revert collision fix

--- src/projectile/projectile_update.c
+++ src/projectile/projectile_update.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 17:53:38 by ahakanen          #+#    #+#             */
-/*   Updated: 2021/04/25 17:14:41 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/04/25 17:22:03 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,33 +120,34 @@ static int		check_projectile_collision_with_player(t_doom3d *app,
 	return (0);
 }
 
-static void		projectile_handle_collision(t_doom3d *app,
+ static void	projectile_handle_collision(t_doom3d *app,
 					t_3d_object *projectile_obj)
-{
-	t_hits		*hits;
-	t_hit		*closest_triangle_hit;
+ {
+	t_vec3		dist;
+	int			i;
 	t_3d_object	*obj;
 
+	i = -1;
 	if (check_projectile_collision_with_player(app, projectile_obj))
-		return ;
-	hits = NULL;
-	if (l3d_kd_tree_ray_hits(app->active_scene->triangle_tree,
-		projectile_obj->position,
-		((t_projectile*)projectile_obj->params)->dir, &hits))
+			return ;
+	while (++i < (int32_t)(app->active_scene->num_objects +
+													app->active_scene->num_deleted))
 	{
-		l3d_get_closest_triangle_hit(hits, &closest_triangle_hit, projectile_obj->id);
-		if (closest_triangle_hit != NULL &&
-			closest_triangle_hit->t < app->unit_size * 1)
-		{
-			obj = closest_triangle_hit->triangle->parent;
-			projectile_explode_effect(app, projectile_obj);
-			push_custom_event(app, event_object_delete,
-				projectile_obj, NULL);
-			projectile_on_hit(app, projectile_obj, obj);
-		}
-		l3d_delete_hits(&hits);
+			obj = app->active_scene->objects[i];
+			if (!obj || obj->type == object_type_projectile ||
+							obj->type == object_type_trigger)
+					continue ;
+			ml_vector3_sub(obj->position, projectile_obj->position, dist);
+			if (ml_vector3_mag(dist) < app->unit_size * 30 &&
+					l3d_aabb_collides(&obj->aabb, &projectile_obj->aabb))
+			{
+					projectile_explode_effect(app, projectile_obj);
+					push_custom_event(app, event_object_delete,
+							projectile_obj, NULL);
+					projectile_on_hit(app, projectile_obj, obj);
+			}
 	}
-}
+ }
 
 void			projectile_update(t_doom3d *app, t_3d_object *projectile_obj)
 {
