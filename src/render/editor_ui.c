ae6556e65a88ba070974118ef278c9a6d380e435
veilo <vesa.eilo@hotmail.com>
2021-04-29 17:39:18 +0300

    updated editor guide text

--- src/render/editor_ui.c
+++ src/render/editor_ui.c
@@ -65,24 +65,24 @@ void			render_guide_on_popup(t_doom3d *app)
 {
 	char	guide[1024];
 
-	ft_sprintf(guide, "Tab: Switch to next level in level list\n"
-		"Ctrl + Tab: Switch to previous level in level list\n"
-		"Button W | A | S | D: Move & Strafe\n"
-		"Mouse Middle / Alt + Mouse Move: Rotate view\n"
-		"Mouse Left: Select Target by mouse\n"
-		"Mouse Left + Shift: Select Multiple By mouse\n"
-		"Mouse Right on target: Deselect one\n"
-		"Mouse Right on empty: Deselect All\n"
-		"Mouse Right on path node: Connect to another node\n"
-		"Button R + X | Y | Z + lshift: Rotate selected (shift slower)\n"
-		"Button Arrows + rshift:"
-		"Move selected on x and z axis, shift slower\n"
-		"Button O | L: Move selected target on y axis, shift slower\n"
-		"Button [ | ] + \\: Scale selected (with uvs if \\)\n"
-		"Button Space: Snap selected to Grid\n"
-		"Button Delete: Delete selected\n"
-		"Button V: Toggle lock vertical movement\n"
-		"Button =/+ | -: Inc/Decrement patrol path node slot\n");
+	ft_sprintf(guide, "Switch to next level in level list: Tab\n"
+		"Switch to previous level in level list: Ctrl + Tab\n"
+		"Move & Strafe: Button W | A | S | D\n"
+		"Rotate view: Mouse Middle / Alt + Mouse Move\n"
+		"Select Target by mouse: Mouse Left\n"
+		"Select Multiple By mouse: Mouse Left + Shift\n"
+		"Deselect one: Mouse Right on target\n"
+		"Deselect All: Mouse Right on empty\n"
+		"Connect to another node: Mouse Right on path node\n"
+		"Rotate selected (shift slower): Button R +  X | Y | Z  + lshift\n"
+		"Move selected on x and z axis, shift slower: Button Arrows + rshift\n"
+		"Move selected target on y axis, shift slower: Button O | L\n"
+		"Scale selected (with uvs if \\): Button [ | ] + \\\n"
+		"Snap selected to Grid: Button Space\n"
+		"Delete selected: Button Delete\n"
+		"Toggle lock vertical movement: Button V\n"
+		"Inc/Decrement patrol path node slot: Button =/+ | -\n"
+		"Toggle darkness when lights are in scene: Button P\n");
 	window_text_render_wrapped_shaded(app->window, (t_text_params){
 		.text = guide, .blend_ratio = 1.0,
 		.xy = (int[2]){app->editor.editor_menu->pos[0] + 10,
