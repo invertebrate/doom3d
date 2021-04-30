e3dcc4e7cea1653c2f80fe7004b05dcc2625498b
hakolao <okkohakola@gmail.com>
2021-04-30 20:42:12 +0300

    Fix crate destroy

--- src/player/player_shoot.c
+++ src/player/player_shoot.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 18:51:46 by ahakanen          #+#    #+#             */
-/*   Updated: 2021/04/30 18:32:59 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/04/30 20:41:09 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,11 @@ static void		handle_shoot_hit(t_doom3d *app, t_hit *closest_triangle_hit,
 	{
 		shoot_bullet_effect(app, closest_triangle_hit, dir,
 			"assets/textures/blood.bmp");
+	}
+	if (!is_blood && closest_triangle_hit->triangle->parent->type ==
+		object_type_npc)
 		npc_trigger_onhit(app, closest_triangle_hit->triangle->parent,
 						app->player.equipped_weapon->damage_per_hit);
-	}
 	if (closest_triangle_hit->triangle->parent->type == object_type_trigger)
 	{
 		trigger_activate(app, closest_triangle_hit->triangle->parent);
