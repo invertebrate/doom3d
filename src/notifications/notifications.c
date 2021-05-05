df7e109150852f5ec79fc8819042b1b5dc8c7883
ahakanen <ahakanen@example.com>
2021-05-05 14:17:18 +0300

    notifications norm fixed

--- src/notifications/notifications.c
+++ src/notifications/notifications.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   notifications.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: sotamursu <sotamursu@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 16:14:00 by ohakola           #+#    #+#             */
-/*   Updated: 2021/04/25 14:31:36 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/05/05 14:12:47 by sotamursu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 ** Note that the distance limit is same as actionable trigger's interact radius
 */
 
-static void		update_in_game_action_notification(t_doom3d *app)
+static void	update_in_game_action_notification(t_doom3d *app)
 {
 	t_hits		*hits;
 	t_hit		*closest_triangle_hit;
 	t_3d_object	*obj;
 	float		distance_limit;
 
-	if (app->active_scene->scene_id != scene_id_main_game ||
-		app->active_scene->is_paused)
+	if (app->active_scene->scene_id != scene_id_main_game
+		|| app->active_scene->is_paused)
 	{
 		ft_memset(app->action_text, 0, sizeof(app->action_text));
 		return ;
@@ -32,12 +32,12 @@ static void		update_in_game_action_notification(t_doom3d *app)
 	distance_limit = app->unit_size * 3;
 	hits = NULL;
 	if (l3d_kd_tree_ray_hits(app->active_scene->triangle_tree, app->player.pos,
-		app->player.forward, &hits))
+			app->player.forward, &hits))
 	{
 		l3d_get_closest_triangle_hit(hits, &closest_triangle_hit, -1);
 		obj = closest_triangle_hit->triangle->parent;
-		if (closest_triangle_hit != NULL && obj->type == object_type_trigger &&
-			closest_triangle_hit->t < distance_limit)
+		if (closest_triangle_hit != NULL && obj->type == object_type_trigger
+			&& closest_triangle_hit->t < distance_limit)
 			get_trigger_action_text(obj->params_type, app->action_text);
 		else
 			ft_memset(app->action_text, 0, sizeof(app->action_text));
@@ -45,7 +45,7 @@ static void		update_in_game_action_notification(t_doom3d *app)
 	}
 }
 
-void			update_notifications(t_doom3d *app)
+void	update_notifications(t_doom3d *app)
 {
 	t_notification	*last_notification;
 	t_list			**last_node;
@@ -66,7 +66,7 @@ void			update_notifications(t_doom3d *app)
 	}
 }
 
-void			notify_user(t_doom3d *app,
+void	notify_user(t_doom3d *app,
 					t_notification notification)
 {
 	if (app->notifications == NULL)
@@ -76,7 +76,7 @@ void			notify_user(t_doom3d *app,
 			ft_lstnew(&notification, sizeof(t_notification)));
 }
 
-void			delete_notifications(t_doom3d *app)
+void	delete_notifications(t_doom3d *app)
 {
 	t_list		*node;
 	t_list		*tmp;
@@ -85,7 +85,7 @@ void			delete_notifications(t_doom3d *app)
 	while (node)
 	{
 		tmp = node;
-		free((t_notification*)node->content);
+		free((t_notification *)node->content);
 		node = node->next;
 		free(tmp);
 	}
