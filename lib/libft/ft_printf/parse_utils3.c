9c5f91382227698410f016d328c7cacf191ca9d3
hakolao <okkohakola@gmail.com>
2021-05-05 13:36:59 +0300

    Fix parse float for long double

--- lib/libft/ft_printf/parse_utils3.c
+++ lib/libft/ft_printf/parse_utils3.c
@@ -1,37 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_round.c                                         :+:      :+:    :+:   */
+/*   parse_utils3.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/18 23:26:36 by ohakola           #+#    #+#             */
-/*   Updated: 2021/05/05 13:13:21 by ohakola          ###   ########.fr       */
+/*   Created: 2021/05/05 13:32:24 by ohakola           #+#    #+#             */
+/*   Updated: 2021/05/05 13:33:52 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
-double	ft_ceil(double num)
+t_dtoa_format	get_float_f_format(t_printf *data)
 {
-	long long int		int_part;
-	t_float_dissector	fd;
+	t_dtoa_format		format;
 
-	if (num == 0)
-		return (num);
-	fd.f = num;
-	int_part = (long long int)num;
-	if (fd.b.sign)
-		return (int_part);
-	return (int_part + 1);
-}
-
-double	ft_floor(double num)
-{
-	t_float_dissector	fd;
-
-	fd.f = num;
-	if (!fd.b.sign)
-		return ((long long int)num);
-	return ((long long int)num - 1);
+	format = FORMAT_SCI;
+	if (data->c == 'f' || data->c == 'F')
+		format = FORMAT_NORM;
+	return (format);
 }
