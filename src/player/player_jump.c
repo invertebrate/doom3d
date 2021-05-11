05ef70f995af01bf58efc28fcf8aaa5ea6442226
veilo <vesa.eilo@hotmail.com>
2021-05-11 18:04:39 +0300

    adjusted player jump

--- src/player/player_jump.c
+++ src/player/player_jump.c
@@ -6,7 +6,7 @@
 /*   By: veilo <veilo@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 18:47:01 by ahakanen          #+#    #+#             */
-/*   Updated: 2021/05/09 16:52:57 by veilo            ###   ########.fr       */
+/*   Updated: 2021/05/11 18:04:06 by veilo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	player_jump(t_doom3d *app)
 {
 	if (app->player.physics_state == physics_state_grounded)
 	{
-		app->player.velocity[1] -= app->player.jump_force * 1.0;
+		app->player.velocity[1] -= app->player.jump_force * 0.9;
+		app->player.velocity[0] = app->player.velocity[0] * 1.5;
+		app->player.velocity[2] = app->player.velocity[2] * 1.5;
 		app->player.physics_state = physics_state_jumping;
 	}
 	if (app->player.can_fly)
