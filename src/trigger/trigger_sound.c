aa38484fa03b0bf52af9009a82e05fd292150443
Petrus Hakakoski <phakakos@student.hive.fi>
2021-05-12 22:50:38 +0300

    Removed music condition from jukebox

--- src/trigger/trigger_sound.c
+++ src/trigger/trigger_sound.c
@@ -19,13 +19,6 @@ void	handle_jukebox(t_doom3d *app, t_3d_object *obj)
 	if (((t_trigger *)(obj->params))->key_id < 0
 		|| ((t_trigger *)(obj->params))->key_id > 19)
 		return ;
-	if (((t_trigger *)(obj->params))->key_id == 17)
-	{
-		LOG_INFO("SHOULD CHANGE MUSIC!");
-		mp_typec(app, 0, 0, SSTOPPED);
-		push_custom_event(app, event_music_play,
-			(void *)mu_doom, s_ini(1, 10, st_main_menu, 0.3));
-	}
 	obj->params_type = trigger_type_disabled;
 	log_id = sf_audio_log_1 + ((t_trigger *)(obj->params))->key_id;
 	push_custom_event(app, event_effect_play, (void *)(intptr_t)log_id,
