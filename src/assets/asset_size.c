b7a5afde3c4e5e81d9b3eb0246adb62e201eff63
hakolao <okkohakola@gmail.com>
2021-05-16 20:34:37 +0300

    Finish asset size count funtion

--- src/assets/asset_size.c
+++ src/assets/asset_size.c
@@ -6,11 +6,23 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 20:23:59 by ohakola           #+#    #+#             */
-/*   Updated: 2021/05/16 20:25:16 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/05/16 20:32:08 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom3d.h"
+
+static size_t	get_3d_obj_size(t_3d_object *obj)
+{
+	size_t	size_count;
+
+	size_count = 0;
+	size_count += obj->num_vertices * sizeof(t_vertex);
+	size_count += obj->num_triangles * sizeof(t_triangle);
+	size_count += sizeof(t_material);
+	size_count += sizeof(t_3d_object);
+	return (size_count);
+}
 
 static void	add_surface_size(void *val, void *params)
 {
@@ -21,6 +33,16 @@ static void	add_surface_size(void *val, void *params)
 	surface = val;
 	*counter += sizeof(t_surface);
 	*counter += sizeof(uint32_t) * surface->w * surface->h;
+}
+
+static void	add_3d_animation_frame_size(void *val, void *params)
+{
+	t_3d_object	*obj;
+	size_t		*counter;
+
+	counter = params;
+	obj = val;
+	*counter += get_3d_obj_size(obj);
 }
 
 static size_t	get_sdl_asset_size(SDL_RWops *asset)
@@ -41,6 +63,8 @@ size_t	get_assets_size(t_assets *assets)
 	hash_map_foreach(assets->hud_textures, add_surface_size, &size_count);
 	hash_map_foreach(assets->textures, add_surface_size, &size_count);
 	hash_map_foreach(assets->normal_maps, add_surface_size, &size_count);
+	hash_map_foreach(assets->animation_3d_frames,
+		add_3d_animation_frame_size, &size_count);
 	i = -1;
 	while (++i < SOUNDS_NUM_TRACKS)
 		size_count += get_sdl_asset_size(assets->sounds[i]);
