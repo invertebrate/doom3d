4bf718a39e89b7d031affcea40d25a664bbe2950
hakolao <okkohakola@gmail.com>
2021-05-19 22:52:05 +0300

    Allow editor when non-asset load mode

--- src/events/custom_event_handling/scene.c
+++ src/events/custom_event_handling/scene.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 16:51:15 by ohakola           #+#    #+#             */
-/*   Updated: 2021/05/15 22:57:26 by ohakola          ###   ########.fr       */
+/*   Updated: 2021/05/19 22:51:19 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,24 @@ void	handle_window_resize(t_doom3d *app, uint32_t width,
 
 void	handle_scene_change(t_doom3d *app, t_scene_id scene_id)
 {
+	int32_t		fd;
+	char		filename[128];
+
 	if (scene_id == scene_id_editor3d)
 	{
-		if (!app->is_asset_load)
+		if (app->is_asset_load)
 		{
-			notify_user(app, (t_notification){.message
-				= "To use editor, you must start app in asset load mode",
-				.time = 3000, .type = notification_type_info});
-			return ;
+			ft_sprintf(filename, "assets/models/box.obj");
+			fd = open(filename, O_RDONLY);
+			if (fd == -1)
+			{
+				notify_user(app, (t_notification){.message
+					= "To use editor, you must launch the app so that the "
+					"assets folder is next to the executable",
+					.time = 4000, .type = notification_type_info});
+				close(fd);
+				return ;
+			}
 		}
 	}
 	app->next_scene_id = scene_id;
